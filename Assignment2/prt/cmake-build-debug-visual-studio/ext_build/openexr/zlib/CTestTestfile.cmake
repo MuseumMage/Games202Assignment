dd9e70190f4f03de0c823c755ad9916c9c166472
zhangbo <mageboer@qq.com>
2024-06-03 18:15:55 +0800

    Apply .gitignore

--- Assignment2/prt/cmake-build-debug-visual-studio/ext_build/openexr/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Project/Games202Assignment/Assignment2/prt/ext/openexr/zlib
-# Build directory: D:/Project/Games202Assignment/Assignment2/prt/cmake-build-debug-visual-studio/ext_build/openexr/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
